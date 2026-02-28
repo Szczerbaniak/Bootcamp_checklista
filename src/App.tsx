6afd19302b74efc2cf2e698aaf641c7a4d50cbc8
Piotr Szczerbaniak <piotr.szczerbaniak@gmail.com>
2026-02-28 21:50:04 +0100

    created task handling logic and needed components folders and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const PriorityLevels = {
+    Low: "low",
+    Medium: "medium",
+    High: "high",
+  } as const;
+
+  type Priority = (typeof PriorityLevels)[keyof typeof PriorityLevels];
+
+  interface Task {
+    content: string;
+    priority: Priority;
+    done: boolean;
+    id: number;
+  }
+
+  const [tasks, setTasks] = useState<Task[]>([
+    { content: "test", priority: "medium", done: false, id: 1 },
+  ]);
+
+  function assignNewIdToTask(currentTasks: Task[]): number {
+    return currentTasks.length > 0
+      ? Math.max(...currentTasks.map((task) => task.id)) + 1
+      : 1;
+  }
+
+  function addTask(newTaskContent: string, taskPriority: Priority) {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        content: newTaskContent,
+        priority: taskPriority,
+        done: false,
+        id: assignNewIdToTask(prevTasks),
+      },
+    ]);
+  }
+
+  function markTaskComplited(id: number): void {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => {
+        if (task.id !== id) {
+          return task;
+        }
+        return {
+          ...task,
+          done: true,
+        };
+      }),
+    );
+  }
+
+  function removeTask(id: number): void {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1>Lista zadań</h1>
+          
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
